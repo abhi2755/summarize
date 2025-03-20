98a47f583618c6792d06786e32754ffa52e36d67
Abhishek <abhi279555@gmail.com>
2025-03-21 02:10:30 +0530

    how it works section comleted still in landing page

--- components/ui/common/how-it-works.tsx
+++ components/ui/common/how-it-works.tsx
@@ -1,3 +1,31 @@
+import { BrainCircuit, FileOutput, FileText, MoveRight } from "lucide-react";
+import { ReactNode } from "react";
+
+type Step = {
+  icon: ReactNode;
+  label: string;
+  description: string;
+};
+
+const steps: Step[] = [
+  {
+    icon: <FileText size={64} strokeWidth={1.5} />,
+    label: "Upload your PDf",
+    description: "Simply drag and drop your PDF document or click to upload",
+  },
+  {
+    icon: <BrainCircuit size={64} strokeWidth={1.5} />,
+    label: "AI Analysis",
+    description:
+      "Our advance AI processes and analyzes your document instantly",
+  },
+  {
+    icon: <FileOutput size={64} strokeWidth={1.5} />,
+    label: "Get Summary",
+    description: "Get a clear, concise summary of your document",
+  },
+];
+
 export default function HowItWorksSection() {
   return (
     <section className="relative bg-gray-50 overflow-hidden">
@@ -15,13 +43,46 @@ export default function HowItWorksSection() {
           />
         </div>
         <div className="text-center mb-16">
-          <h2>How it works</h2>
-          <h3>
+          <h2 className="font-bold text-xl uppercase mb-4 text-rose-500">
+            How it works
+          </h2>
+          <h3 className="font-bold text-3xl max-w-2xl mx-auto">
             Transform any PDF into easy-to-digest summary in three simple steps
           </h3>
         </div>
-        <div className="grid grid-cols-1 md:grid-cols-3 gap-8 max-w-6xl mx-auto relative"></div>
+        <div className="grid grid-cols-1 md:grid-cols-3 gap-8 max-w-6xl mx-auto relative">
+          {steps.map((step, idx) => (
+            <div className="relative flex items-stretch">
+              <StepItem key={idx} {...step} />
+              {idx < steps.length - 1 && (
+                <div className="hidden md:block absolute top-1/2 -right-4 transform -translate-y-1/2 z-10">
+                  <MoveRight
+                    size={32}
+                    strokeWidth={1}
+                    className="text-red-400"
+                  />
+                </div>
+              )}
+            </div>
+          ))}
+        </div>
       </div>
     </section>
+  );
+}
+
+function StepItem({ icon, label, description }: Step) {
+  return (
+    <div className="relative p-6 rounded-2xl bg-white/5 backdrop-blur-xs border border-white/10 hover:border-rose-500/5 transition-colors group w-full">
+      <div className="flex flex-col h-full gap-4">
+        <div className="flex items-center justify-center h-24 w-24 mx-auto rounded-2xl bg-linear-to-br from-rose-500/10 to-transparent group-hover:from-rose-500/20 transition-colors">
+          <div className="text-rose-500">{icon}</div>
+        </div>
+        <div className="flex flex-col flex-1 gap-1 justify-between">
+          <h4 className="text-center font-bold text-xl">{label}</h4>
+          <p className="text-center text-sm text-gray-600">{description}</p>
+        </div>
+      </div>
+    </div>
   );
 }
